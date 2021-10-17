aa3802243d85ff00ff76751310de746a3b1f6ad3
Gabriel Kressin <gabriel.kressin@fu-berlin.de>
2021-10-17 02:26:23 +0200

    removed website stats, adapted personal stats

--- src/data/stats/personal.js
+++ src/data/stats/personal.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import dayjs from 'dayjs';
 
 const Age = () => {
   const [age, setAge] = useState();
 
   const tick = () => {
     const divisor = 1000 * 60 * 60 * 24 * 365.2421897; // ms in an average year
-    const birthTime = new Date('1990-02-05T09:24:00');
+    const birthTime = new Date('1998-09-13T09:24:00');
     setAge(((Date.now() - birthTime) / divisor).toFixed(11));
   };
 
@@ -25,17 +26,33 @@ const data = [
     value: <Age />,
   },
   {
-    key: 'countries',
-    label: 'Countries visited',
-    value: 53,
-    link:
-      'https://www.google.com/maps/d/embed?mid=1iBBTscqateQ93pWFVfHCUZXoDu8&z=2',
+    key: 'nationalities',
+    label: 'Nationalities',
+    value: 3,
   },
   {
     key: 'location',
     label: 'Current city',
-    value: 'New York, NY',
+    value: 'Berlin, Germany',
+    link: 'https://www.berlin.de/en/',
+  },
+  {
+    key: 'transport',
+    label: 'Main mode of transport',
+    value: 'Bike',
+  },
+  {
+    key: 'push-ups',
+    label: 'All-time record of push-ups',
+    value: '42',
+  },
+  {
+    label: 'Website last updated at',
+    key: 'pushed_at',
+    link: 'https://github.com/GabrielKP/gabrielkp.github.io/commits',
+    format: (x) => dayjs(x).format('MMMM DD, YYYY'),
   },
 ];
+// Add photo of my bike
 
 export default data;
