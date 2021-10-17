3e20f680f3a7a15d389eb58ca9457b23f5011054
Gabriel Kressin <gabriel.kressin@fu-berlin.de>
2021-10-17 12:08:04 +0200

    swapped about react component with activities component

---
+++ src/pages/Activities.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Main from '../layouts/Main';
+
+import Cell from '../components/Projects/Cell';
+import data from '../data/projects';
+
+const Activities = () => (
+  <Main
+    title="Activities"
+    description="Learn about Gabriel Kressin's activities."
+  >
+    <article className="post" id="projects">
+      <header>
+        <div className="title">
+          <h2 data-testid="heading"><Link to="/activities">Activities</Link></h2>
+          <p>Things I do in my free time</p>
+        </div>
+      </header>
+      {data.map((project) => (
+        <Cell
+          data={project}
+          key={project.title}
+        />
+      ))}
+    </article>
+  </Main>
+);
+
+export default Activities;
